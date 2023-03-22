0cf0e95dad5b5a9b126581982bb23b9f787fd6aa
enriquesb <enriquesb1@gmail.com>
2023-03-22 00:08:50 -0500

    Clean commented code lines

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -7,32 +7,12 @@ export default function NewTaskForm({ handleSubmit }) {
     setTaskInput(e.target.value);
   }
 
-  // function handleSubmit() {
-  //   const currentTasksArray = JSON.parse(localStorage.getItem("tasksArray"));
-  //   let nextId;
-  //   if (currentTasksArray.length === 0) {
-  //     nextId = 0;
-  //   } else {
-  //     nextId = currentTasksArray.slice(-1)[0].id + 1;
-  //   }
-  //   const newTaskArray = [
-  //     ...currentTasksArray,
-  //     {
-  //       id: nextId,
-  //       text: taskInput,
-  //       crossOut: false,
-  //     },
-  //   ];
-  //   localStorage.setItem("tasksArray", JSON.stringify(newTaskArray));
-  // }
-
   return (
     <div>
       <p>Hola mundo</p>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          // handleSubmit();
           handleSubmit(taskInput);
           setTaskInput("");
         }}
